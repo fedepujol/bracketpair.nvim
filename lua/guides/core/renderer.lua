18e2a1c0c95a487c814a9df2aa82efdcaea26411
fedepujol <9yjbd325@anonaddy.me>
2023-05-05 17:29:06 -0300

    refactor: changes made in renderer an utils

--- lua/guides/core/renderer.lua
+++ lua/guides/core/renderer.lua
@@ -1,6 +1,5 @@
 local utils = require('guides.utils')
 
-local nsid = vim.api.nvim_create_namespace('bracket')
 local nsid_opts = {
 	virt_text_pos = 'overlay',
 	hl_mode = 'combine',
@@ -14,15 +13,46 @@ local line_guides = {
 
 local M = {}
 
+local nsid = vim.api.nvim_create_namespace('bracket')
+
+M.state = {
+	prev_start = 0,
+	prev_stop = 0,
+	prev_line = 0
+}
+
+---
+---@param start number
+---@param stop number
+---@param line number
+M.update_state = function(start, stop, line)
+	M.state.prev_start = start
+	M.state.prev_stop = stop
+	M.state.prev_line = line
+end
+
 M.bracket = function()
+	local cCursor = vim.api.nvim_win_get_cursor(0)
+	local cLine = cCursor[1]
+	local cCol = cCursor[2]
+
+	if utils.should_skip_char(cCol) then
+		return
+	end
+
+	-- Get the start and end position of the bracket
+	local positions = utils.findPairs(cLine, cCol)
+
+	if positions.start_pos.line == M.state.prev_start and
+		positions.end_pos.line == M.state.prev_stop then
+		return
+	end
+
 	-- Remove previously created extmarks
 	utils.clear_namespace(nsid)
 
-	-- Get the start and end position of the bracket
-	local positions = utils.findPairs()
-
 	-- Edge case
-	if positions.start_pos.line == 0 or positions.end_pos.line == 0 then
+	if positions.start_pos.line == 0 and positions.end_pos.line == 0 then
 		return
 	end
 
@@ -33,28 +63,40 @@ M.bracket = function()
 	start_line = utils.replace_line_content(start_line, space_text)
 	end_line = utils.replace_line_content(end_line, space_text)
 
+	M.update_state(positions.start_pos.line, positions.end_pos.line, cLine)
+
 	local length_start = string.len(start_line)
 	local length_end = string.len(end_line)
 	local space_len = math.min(length_start - vim.fn.shiftwidth(), length_end - vim.fn.shiftwidth())
 	local indent = utils.line_indent(positions.start_pos.line)
 
+	if indent == 0 then
+		return
+	end
+
 	space_len = math.max(space_len, 0)
 
 	if length_start == 1 then
 		local virtual_text = line_guides[1]
+
 		nsid_opts.virt_text = { { virtual_text, 'BracketUnderline' } }
 		nsid_opts.virt_text_win_col = indent * vim.fn.shiftwidth()
 		vim.api.nvim_buf_set_extmark(0, nsid, positions.start_pos.line - 1, 0, nsid_opts)
 	end
+
 	if length_start >= 2 then
 		local aux_start_line = string.sub(start_line, (indent * vim.fn.shiftwidth()) + 1)
 		local offset = indent * vim.fn.shiftwidth()
+
 		if positions.start_pos.line == positions.end_pos.line then
 			aux_start_line = string.sub(start_line, positions.start_pos.col + (indent * vim.fn.shiftwidth()) - 2,
 				positions.end_pos.col + (indent * vim.fn.shiftwidth()) - 2)
-			offset = string.len(string.sub(start_line, 1, positions.start_pos.col + (indent * vim.fn.shiftwidth()) - 2)) - 1
+			offset = string.len(string.sub(start_line, 1,
+				positions.start_pos.col + (indent * vim.fn.shiftwidth()) - 2)) - 1
 		end
+
 		local virtual_text = aux_start_line
+
 		nsid_opts.virt_text = { { virtual_text, 'BracketUnderline' } }
 		nsid_opts.virt_text_win_col = offset
 		vim.api.nvim_buf_set_extmark(0, nsid, positions.start_pos.line - 1, 0, nsid_opts)
@@ -62,12 +104,15 @@ M.bracket = function()
 
 	if length_end == 1 then
 		local virtual_text = end_line
+
 		nsid_opts.virt_text = { { virtual_text, 'BracketUnderline' } }
 		nsid_opts.virt_text_win_col = indent * vim.fn.shiftwidth()
 		vim.api.nvim_buf_set_extmark(0, nsid, positions.end_pos.line - 1, 0, nsid_opts)
 	end
+
 	if length_end >= 2 then
 		local virtual_text = string.sub(end_line, indent * vim.fn.shiftwidth() + 1)
+
 		nsid_opts.virt_text = { { virtual_text, 'BracketUnderline' } }
 		nsid_opts.virt_text_win_col = indent * vim.fn.shiftwidth()
 		vim.api.nvim_buf_set_extmark(0, nsid, positions.end_pos.line - 1, 0, nsid_opts)
@@ -79,9 +124,11 @@ M.bracket = function()
 	for i = edges.visible_start + 1, edges.visible_end - 1, 1 do
 		local c_line = vim.api.nvim_buf_get_lines(0, i - 1, i, true)[1]
 		c_line = string.gsub(c_line, '\t', space_text)
+
 		if string.len(c_line) == 0 then
 			c_line = c_line .. utils.getChars(indent * vim.fn.shiftwidth(), ' ')
 		end
+
 		if string.sub(c_line, space_len, 1) ~= '\\s' or string.len(c_line) <= space_len then
 			local virtual_text = line_guides[3]
 			nsid_opts.virt_text = { { virtual_text, 'BracketLine' } }
@@ -93,5 +140,3 @@ M.bracket = function()
 end
 
 return M
-
-
--- lua/guides/utils/init.lua
+++ lua/guides/utils/init.lua
@@ -3,26 +3,28 @@ local M = {}
 
 ---Set bracket highlights groups
 M.setup_hl = function()
-	local result, hl = pcall(vim.api.nvim_get_hl_by_name, 'MatchParen', true)
+	local _, hl = pcall(vim.api.nvim_get_hl, 0, { name = 'MatchParen', link = true })
 	local color = hl.foreground or "cyan"
 
-	vim.api.nvim_set_hl(0, 'BracketUnderline', { sp = color, underline = true } )
+	vim.api.nvim_set_hl(0, 'BracketUnderline', { sp = color, underline = true })
 	vim.api.nvim_set_hl(0, 'BracketLine', { fg = color, underline = false })
 end
 
 ---@param lineNr number
+---@return number
 M.line_indent = function(lineNr)
 	return vim.fn.indent(lineNr) / vim.fn.shiftwidth()
 end
 
 ---Finds the position (line, col) of the curly-brackets
+---@param line number current line number
+---@param col number current column number
 ---@return table
-M.findPairs = function()
-	local cCursor = vim.api.nvim_win_get_cursor(0)
-	local cLine = vim.api.nvim_buf_get_lines(0, cCursor[1] - 1, cCursor[1], true)[1]
+M.findPairs = function(line, col)
+	local cLine = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
 
-	local sPos = vim.fn.searchpairpos('{', '', '}', 'znWb' .. (string.sub(cLine, cCursor[2], 1) == '{' and 'c' or ''))
-	local ePos = vim.fn.searchpairpos('{', '', '}', 'znW' .. (string.sub(cLine, cCursor[2], 1) == '}' and 'c' or ''))
+	local sPos = vim.fn.searchpairpos('{', '', '}', 'znWb' .. (string.sub(cLine, col, 1) == '{' and 'c' or ''))
+	local ePos = vim.fn.searchpairpos('{', '', '}', 'znW' .. (string.sub(cLine, col, 1) == '}' and 'c' or ''))
 	return {
 		start_pos = {
 			line = sPos[1],
@@ -53,6 +55,9 @@ M.clear_namespace = function(nsid)
 	vim.api.nvim_buf_clear_namespace(0, nsid, 0, -1)
 end
 
+---@param line string
+---@param space_text string
+---@return string
 M.replace_line_content = function(line, space_text)
 	line = string.gsub(line, '\v^(\\s*).*', '\\1')
 	line = string.gsub(line, '\t', space_text)
@@ -74,6 +79,17 @@ M.calc_visibleEdges = function(positions)
 		visible_start = visible_start,
 		visible_end = visible_end
 	}
+end
+
+---
+---@param temp_col number
+---@return boolean
+M.should_skip_char = function(temp_col)
+	local col = temp_col + 1
+	local line = vim.api.nvim_get_current_line()
+	local cChar = string.sub(line, col, col)
+
+	return cChar == '}' or cChar == '{'
 end
 
 return M
